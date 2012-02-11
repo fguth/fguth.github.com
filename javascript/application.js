080db03e12200701e658475b499fd220ba4d2f2c
Filipe Guth <lfguth@gmail.com>
2012-02-11 03:46:28 -0200

    new layout and sructure

--- javascript/application.js
+++ javascript/application.js
@@ -1,8 +1,15 @@
 $(function() {
-	$('.work').mouseover(function() {
-  	$(this).find('a').show();
+	$('.work').click(function(){
+		if($(this).hasClass('opened')) {
+		  $(this).removeClass('opened');
+		}
+		else {
+		  $(this).addClass('opened');
+		}
 	});
-	$('.work').mouseout(function() {
-  	$(this).find('a').hide();
+	
+	$('#find-fguth ul li a').hover(function(){ 
+		$('div.tipsy.' + $(this).attr('class')).toggleClass('hover');
 	});
+	
 });
