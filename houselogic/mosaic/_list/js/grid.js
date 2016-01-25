c2e7c97b4bf019325722ccc43cd0fe21b8b1dddd
Filipe Guth <lfguth@gmail.com>
2016-01-25 11:34:30 -0200

    new mosaic stuff

---
+++ houselogic/mosaic/_list/js/grid.js
@@ -0,0 +1,20 @@
+function sizing() {
+  var divWidth = $('.height').width();
+  var divSmallWidth = $('.height-small').width();
+  var articleHeight = $('.height').width() * 1.6 - $('.height').width() / 4;
+  var articleSmallHeight = $('.height-small').width() * 2 - $('.height-small').width() / 4;
+
+  $('.image-holder').height(divWidth);
+  $('.article-image-mask').height(divWidth);
+  
+  $('.height-small .image-holder').height(divSmallWidth);
+  $('.height-small .article-image-mask').height(divSmallWidth);
+  
+  $('.height').height(articleHeight);
+  $('height-small').height(articleSmallHeight);
+}
+
+$(document).ready(sizing);
+$(window).resize(sizing);
+
+
---
+++ houselogic/mosaic/_smooth-color/js/grid.js
@@ -0,0 +1,19 @@
+function sizing() {
+  var divWidth = $('.height').width();
+  var divSmallWidth = $('.height-small').width();
+  var articleHeight = $('.height').width() * 2 - $('.height').width() / 4;
+  var articleSmallHeight = $('.height-small').width() * 2 - $('.height-small').width() / 4;
+
+  $('.image-holder').height(divWidth);
+  $('.article-image-mask').height(divWidth);
+  
+  $('.height-small .image-holder').height(divSmallWidth);
+  $('.height-small .article-image-mask').height(divSmallWidth);
+  
+//  $('height').height(articleHeight);
+//  $('height-small').height(articleSmallHeight);
+}
+
+$(document).ready(sizing);
+$(window).resize(sizing);
+
