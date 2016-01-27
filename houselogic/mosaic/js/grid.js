e6bd6654ed197dab519b56f0ca0b72f8b8f5692b
Filipe Guth <lfguth@gmail.com>
2016-01-27 17:17:38 -0200

    new mosaic

--- houselogic/mosaic/js/grid.js
+++ houselogic/mosaic/js/grid.js
@@ -1,8 +1,8 @@
 function sizing() {
   var divWidth = $('.height').width();
   var divSmallWidth = $('.height-small').width();
-  var articleHeight = $('.height').width() * 2 - $('.height').width() / 4;
-  var articleSmallHeight = $('.height-small').width() * 2 - $('.height-small').width() / 4;
+  var articleHeight = $('.height').width() * 1.6 - $('.height').width() / 5;
+  var articleSmallHeight = $('.height-small').width() * 2 - $('.height-small').width() / 5.2;
 
   $('.image-holder').height(divWidth);
   $('.article-image-mask').height(divWidth);
@@ -10,27 +10,11 @@ function sizing() {
   $('.height-small .image-holder').height(divSmallWidth);
   $('.height-small .article-image-mask').height(divSmallWidth);
   
-  $('height').height(articleHeight);
-  $('height-small').height(articleSmallHeight);
+  $('.height').height(articleHeight);
+  $('.height-small-image').height(articleSmallHeight);
 }
 
 $(document).ready(sizing);
 $(window).resize(sizing);
-
-
-function sticky_relocate() {
-    var window_top = $(window).scrollTop();
-    var div_top = $('.menu').offset().top;
-    if (window_top > 294) {
-        $('.menu').addClass('stick');
-    } else {
-        $('.menu').removeClass('stick');
-    }
-}
-
-$(function () {
-    $(window).scroll(sticky_relocate);
-    sticky_relocate();
-});
 
 
